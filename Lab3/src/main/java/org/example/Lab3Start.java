290174683f5e6008fc9a5c8e23079811f064ad91
pachkov-nikita <nikitapachkov12345@gmail.com>
2022-12-03 15:05:09 +0200

    Make spring and pom

---
+++ Lab3/src/main/java/org/example/Lab3Start.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Start {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3Start.class, args);
+    }
+}
